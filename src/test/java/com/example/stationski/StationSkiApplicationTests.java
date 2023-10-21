0e656e647c97c97a7dfca110a7c7ad96fbdd6192
naderjardak <naderjardak5@gmail.com>
2023-10-21 13:17:41 +0100

    new import deleted

---
+++ src/test/java/com/example/stationski/StationSkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stationski;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class StationSkiApplicationTests {
+
+   @Test
+    void contextLoads()
+   {}
+
+}
--- src/test/java/com/example/stationski/services/SkieurServiceMockTest.java
+++ src/test/java/com/example/stationski/services/SkieurServiceMockTest.java
@@ -7,16 +7,11 @@ import com.example.stationski.entities.model.SkieurModel;
 import com.example.stationski.repositories.CoursRepository;
 import com.example.stationski.repositories.PisteRepository;
 import com.example.stationski.repositories.SkieurRepository;
-import com.example.stationski.services.SkieurService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
