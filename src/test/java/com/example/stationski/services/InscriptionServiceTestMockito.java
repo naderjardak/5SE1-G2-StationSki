5cae9fe6462bbdf9ccb85b1f0f783df6ecf3c8a8
Tessnime Kabbous <tessnime.kabbous@esprit.tn>
2023-10-22 02:03:22 +0100

    createOthertestunitaire

--- src/test/java/com/example/stationski/services/InscriptionServiceTestMockito.java
+++ src/test/java/com/example/stationski/services/InscriptionServiceTestMockito.java
@@ -5,6 +5,7 @@ import com.example.stationski.repositories.CoursRepository;
 import com.example.stationski.repositories.InscriptionRepository;
 import com.example.stationski.repositories.SkieurRepository;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -13,10 +14,12 @@ import org.mockito.InjectMocks;
 
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-
+import java.time.LocalDate;
 import java.util.Collections;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -24,6 +27,7 @@ import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
+@Slf4j
 public class InscriptionServiceTestMockito {
 
     @InjectMocks
@@ -37,6 +41,9 @@ public class InscriptionServiceTestMockito {
 
     @Mock
     private CoursRepository coursRepository;
+
+
+
 
 
     @Test
@@ -69,24 +76,108 @@ public class InscriptionServiceTestMockito {
         assertEquals(cours, result.getCours());
         assertEquals(1L, result.getCours().getNumCours());
     }
+
     @Test
-    public void testAddInscriptionAndAssignToSkieurAndCourse() {
-        // Créez des objets fictifs pour les paramètres de la méthode
+    public void testAddInscriptionAndAssignToSkieurAndCourse_EmptyDatabase() {
+
+
+        when(coursRepository.findByNumCours(Mockito.anyLong())).thenReturn(null);
+        when(skieurRepository.findByNumSkieur(Mockito.anyLong())).thenReturn(null);
+
         Inscription inscription = new Inscription();
-        Long numSkieur = 1L;
-        Long numCours = 2L;
-
-        // Définissez le comportement des objets mock (coursRepository, skieurRepository, inscriptionRepository)
-        when(coursRepository.findByNumCours(numCours)).thenReturn(new Cours()); // Remplacez par votre implémentation réelle
-        when(skieurRepository.findByNumSkieur(numSkieur)).thenReturn(new Skieur()); // Remplacez par votre implémentation réelle
+        Inscription result = inscriptionService.addInscriptionAndAssignToSkieurAndCourse(inscription, 1L, 1L);
 
 
-        Inscription result = inscriptionService.addInscriptionAndAssignToSkieurAndCourse(inscription, numSkieur, numCours);
         assertNull(result);
-
-
-
     }
+
+    @Test
+    public void testAddInscriptionAndAssignToSkieurAndCourse_CourseNotFound() {
+        Skieur skieur = new Skieur();
+        Inscription inscription = new Inscription();
+
+        when(coursRepository.findByNumCours(anyLong())).thenReturn(null);
+        when(skieurRepository.findByNumSkieur(anyLong())).thenReturn(skieur);
+
+        Inscription result = inscriptionService.addInscriptionAndAssignToSkieurAndCourse(inscription, 1L, 1L);
+
+        assertNull(result);
+    }
+
+    @Test
+    public void testAddInscriptionAndAssignToSkieurAndCourse_SkieurNotFound() {
+        Cours cours = new Cours();
+        Inscription inscription = new Inscription();
+
+        when(coursRepository.findByNumCours(anyLong())).thenReturn(cours);
+        when(skieurRepository.findByNumSkieur(anyLong())).thenReturn(null);
+
+        Inscription result = inscriptionService.addInscriptionAndAssignToSkieurAndCourse(inscription, 1L, 1L);
+
+        assertNull(result);
+    }
+
+    @Test
+    public void testAddInscriptionAndAssignToSkieurAndCourse_IncompatibleAge() {
+        Cours cours = new Cours();
+        Skieur skieur = new Skieur();
+        skieur.setDateNaissance(LocalDate.of(2000, 1, 1)); // Supposez que le skieur est né en 2000.
+        Inscription inscription = new Inscription();
+
+        when(coursRepository.findByNumCours(anyLong())).thenReturn(cours);
+        when(skieurRepository.findByNumSkieur(anyLong())).thenReturn(skieur);
+
+        Inscription result = inscriptionService.addInscriptionAndAssignToSkieurAndCourse(inscription, 1L, 1L);
+
+        assertNull(result);
+    }
+
+    @Test
+    public void testAddInscriptionAndAssignToSkieurAndCourse_MaxInscriptionsReached() {
+        Cours cours = new Cours();
+        Skieur skieur = new Skieur();
+        Inscription inscription = new Inscription();
+
+
+        Set<Inscription> existingInscriptions = IntStream.range(1, 7)
+                .mapToObj(i -> new Inscription())
+                .collect(Collectors.toSet());
+        cours.setInscriptions(existingInscriptions);
+
+        when(coursRepository.findByNumCours(anyLong())).thenReturn(cours);
+        when(skieurRepository.findByNumSkieur(anyLong())).thenReturn(skieur);
+
+        Inscription result = inscriptionService.addInscriptionAndAssignToSkieurAndCourse(inscription, 1L, 1L);
+
+        assertNull(result);
+        log.info("Le nombre d'inscriptions en cours est " + cours.getInscriptions().size());
+    }
+
+
+    @Test
+    public void testNumWeeksCoursOfMoniteurBySupport() {
+      
+        Long numInstructor = 1L;
+        Support support = Support.SKI;
+
+
+        List<Integer> numWeeks = List.of(1,2,3);
+
+
+        when(inscriptionRepository.numWeeksCoursOfMoniteurBySupport(numInstructor, support)).thenReturn(numWeeks);
+
+
+        List<Integer> result = inscriptionService.numWeeksCoursOfMoniteurBySupport(numInstructor, support);
+
+
+        assertEquals(numWeeks, result);
+
+
+        verify(inscriptionRepository).numWeeksCoursOfMoniteurBySupport(numInstructor, support);
+    }
+
+
+
 
 }
 
