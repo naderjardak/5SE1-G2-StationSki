97cf95359ae254fa6e3a73037633ec9de4113865
naderjardak <naderjardak5@gmail.com>
2023-10-30 04:05:47 +0100

    cross error

--- src/main/java/com/example/stationski/controllers/AbonnementController.java
+++ src/main/java/com/example/stationski/controllers/AbonnementController.java
@@ -14,6 +14,7 @@ import java.util.Set;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin(origins = "*")
 @RequestMapping("/abonnement")
 public class AbonnementController {
     @Autowired
--- src/main/java/com/example/stationski/controllers/SkieurController.java
+++ src/main/java/com/example/stationski/controllers/SkieurController.java
@@ -14,6 +14,7 @@ import java.util.Map;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin(origins = "*")
 @RequestMapping("skieur")
 public class SkieurController {
     @Autowired
