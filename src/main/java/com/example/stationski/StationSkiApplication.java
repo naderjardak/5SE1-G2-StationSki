a339adb87a8b0606cd5d51404886e9a507d37e84
naderjardak <naderjardak5@gmail.com>
2023-11-01 17:48:44 +0100

    prometheus

--- src/main/java/com/example/stationski/StationSkiApplication.java
+++ src/main/java/com/example/stationski/StationSkiApplication.java
@@ -1,17 +1,30 @@
 package com.example.stationski;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableAspectJAutoProxy
 public class StationSkiApplication {
 
+    final static Logger logger = LoggerFactory.getLogger(StationSkiApplication.class);
+
+
     public static void main(String[] args) {
         SpringApplication.run(StationSkiApplication.class, args);
+    }
+
+    @GetMapping("/something")
+    public ResponseEntity<String> createLogs() {
+        logger.warn("Just checking");
+        return ResponseEntity.ok().body("All Ok");
     }
 
 }
