2b47f5c48c0b4102d67496f61f2b1964569cc12e
ibrahim12365 <ibrahim.benkhalifa@esprit.tn>
2023-11-07 13:48:41 +0100

    correct CoursControllers

--- src/main/java/com/example/stationski/controllers/CoursController.java
+++ src/main/java/com/example/stationski/controllers/CoursController.java
@@ -2,15 +2,17 @@ package com.example.stationski.controllers;
 
 import com.example.stationski.entities.*;
 import com.example.stationski.services.ICoursService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
 
 import java.util.Set;
-
+@RestController
+@AllArgsConstructor
+@CrossOrigin(origins = "*")
+@RequestMapping("cours")
 public class CoursController {
     @Autowired
     ICoursService iCoursService;
