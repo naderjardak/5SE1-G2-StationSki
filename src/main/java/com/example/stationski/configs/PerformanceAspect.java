0351916df03b77fd91ccc17ca38738e96dfdbd1d
MohsenFenira <mohsen@megatransact.com>
2023-11-09 22:51:35 +0100

    upd

--- src/main/java/com/example/stationski/configs/PerformanceAspect.java
+++ src/main/java/com/example/stationski/configs/PerformanceAspect.java
@@ -7,7 +7,7 @@ import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 
-@Component
+
 @Aspect
 @Slf4j
 public class PerformanceAspect {
--- src/main/java/com/example/stationski/configs/UtilityFonction.java
+++ src/main/java/com/example/stationski/configs/UtilityFonction.java
@@ -5,11 +5,15 @@ import java.time.Period;
 
 public class UtilityFonction {
 
+    private UtilityFonction() {
+        throw new AssertionError("This class cannot be instantiated.");
+    }
+
     public static int calculateAge(LocalDate dob)
     {
 
         LocalDate curDate = LocalDate.now();
-        if ((dob != null) && (curDate != null))
+        if (dob != null)
         {
             return Period.between(dob, curDate).getYears();
         }
