84900b34c3b727c3145aff1c08421b8240979afc
Tessnime Kabbous <tessnime.kabbous@esprit.tn>
2023-11-09 19:35:42 +0100

    testt

--- src/main/java/com/example/stationski/services/AbonnementService.java
+++ src/main/java/com/example/stationski/services/AbonnementService.java
@@ -37,25 +37,5 @@ public class AbonnementService implements IAbonnementService{
         return abonnementRepository.save(abonnement);
     }
 
-    //@Override
-//    @Scheduled(cron = "*/30 * * * * *") /* Cron expression to run a job every 30 secondes */
-  /*  public void retrieveAbonnements() {
-        LocalDate today = LocalDate.now();
-        LocalDate dateExpiration =today.plusDays(7);
-        log.info("dateExpiration : "+dateExpiration);
-        for (Abonnement abonnement: abonnementRepository.findDistinctOrderByDateFinAsc(dateExpiration)) {
-            Skieur aSkier = skieurRepository.findByAbonnement(abonnement);
-            log.info("num abonnement : "+abonnement.getNumAbon().toString() + " | date fin Abonnement :"+ abonnement.getDateFin().toString()
-                    + " |num skieur : "+ aSkier.getNumSkieur()     + " | prenom :"+ aSkier.getPrenomS() + "|nom:  " + aSkier.getNomS());
-        }
-    }
-*/
-    // @Scheduled(cron = "* 0 9 1 * *") /* Cron expression to run a job every month at 9am */
-    //  @Scheduled(cron = "*/30 * * * * *") /* Cron expression to run a job every 30 secondes */
-   /* public void showMonthlyRecurringRevenue() {
-        Float revenue = abonnementRepository.recurringRevenueByTypeSubEquals(TypeAbonnement.MENSUEL)
-                + abonnementRepository.recurringRevenueByTypeSubEquals(TypeAbonnement.SEMESTRIEL)/6
-                + abonnementRepository.recurringRevenueByTypeSubEquals(TypeAbonnement.ANNUEL)/12;
-        log.info("Monthly Revenue = " + revenue);
-    }*/
+
 }
