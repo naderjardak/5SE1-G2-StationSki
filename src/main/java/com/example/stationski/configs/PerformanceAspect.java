e342e71ec2927482070077c2d0a50beeef2e618e
naderjardak <naderjardak5@gmail.com>
2023-11-13 22:16:28 +0100

    cov

--- src/main/java/com/example/stationski/configs/PerformanceAspect.java
+++ src/main/java/com/example/stationski/configs/PerformanceAspect.java
@@ -5,8 +5,6 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 
-import static java.lang.Thread.sleep;
-
 
 @Aspect
 @Slf4j
