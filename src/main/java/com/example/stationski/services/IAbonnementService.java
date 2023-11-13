6ddc8ddd74fcd5d70ee3fc26b150a98ed81727b9
MohsenFenira <mohsen@megatransact.com>
2023-11-14 00:40:36 +0100

    clean project

--- src/main/java/com/example/stationski/services/IAbonnementService.java
+++ src/main/java/com/example/stationski/services/IAbonnementService.java
@@ -18,5 +18,4 @@ public interface IAbonnementService {
     Abonnement addAboonement(AbonnementDto abonnementDto);
     public List<Abonnement> getAllAbonnement();
 
- //   public void retrieveAbonnements();
 }
